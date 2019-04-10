05c76710c92108c4bd9d95e5941462bc5fb6d81c
Adnan <adnanmh82@gmail.com>
2019-04-10 10:08:43 +0100

    Completed A manipulation

--- week-5/A-dom-manipulation/exercise.js
+++ week-5/A-dom-manipulation/exercise.js
@@ -14,10 +14,10 @@ Task 2
 When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
 */
 function alertSomething() {
-  alert("Thank you fot visiting Bilkes for refugees");
+  alert("Thank you fot visiting Bilkes for Refugees");
 }
-var newBotton = document.querySelector("#alertBtn");
-newBotton.addEventListener("click", alertSomething);
+var newButton = document.querySelector("#alertBtn");
+newButton.addEventListener("click", alertSomething);
 
 /*
 Task 3
